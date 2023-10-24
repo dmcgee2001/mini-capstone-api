9563150f3fcca940aa549103c60285582c706427
Derek McGee <dmcgee2001@gmail.com>
2023-10-24 12:35:27 -0400

    added seeds to create products

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+product1 = Product.create(name: "pencil", price: 2.99, image_url: "", description: "this product can write and erase")
+product2 = Product.create(name: "whiteboard", price: 20.99, image_url: "", description: "this product can be drawn on with eraseable marker")
+product3 = Product.create(name: "car", price: 29000, image_url: "", description: "this product can get you from place to place")
