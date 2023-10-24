9cfb46dab1e751fb6ef8ae3d2abec33a56f59965
Derek McGee <dmcgee2001@gmail.com>
2023-10-24 13:51:25 -0400

    adds routes for all products using jbuilder syntax and also displays products 2 and 3 in routes and controllers

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,27 @@
 class ProductsController < ApplicationController
   def one_product
-    product = Product.first
-    render json: product
+    @product = Product.first
+    render :show
+  end
+
+  def one_product2
+    product2 = Product.second
+    render json: {
+             id: product2.id,
+             name: product2.name,
+             price: product2.price,
+             image_url: product2.image_url,
+             description: product2.description,
+           }
+  end
+
+  def one_product3
+    product3 = Product.third
+    render json: product3
+  end
+
+  def all_products
+    @products = Product.all
+    render :index
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   get "/one_product", controller: "products", action: "one_product"
+  get "/one_product2", controller: "products", action: "one_product2"
+  get "/one_product3", controller: "products", action: "one_product3"
+  get "all_products", controller: "products", action: "all_products"
 end
