fa7586a530ad95547ce28411df8998c5622b7090
Derek McGee <dmcgee2001@gmail.com>
2023-10-24 15:11:42 -0400

    adds parameter id to condense all products to one route based on their id

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,23 +1,7 @@
 class ProductsController < ApplicationController
   def one_product
-    @product = Product.first
+    @product = Product.find_by(id: params["id"])
     render :show
-  end
-
-  def one_product2
-    product2 = Product.second
-    render json: {
-             id: product2.id,
-             name: product2.name,
-             price: product2.price,
-             image_url: product2.image_url,
-             description: product2.description,
-           }
-  end
-
-  def one_product3
-    product3 = Product.third
-    render json: product3
   end
 
   def all_products
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get "/one_product", controller: "products", action: "one_product"
-  get "/one_product2", controller: "products", action: "one_product2"
-  get "/one_product3", controller: "products", action: "one_product3"
+  get "/one_product/:id", controller: "products", action: "one_product"
   get "all_products", controller: "products", action: "all_products"
   get "random_product", controller: "products", action: "random"
 end
