0c89ecd314703cc689aa56fc382d7a1963b04c26
Derek McGee <dmcgee2001@gmail.com>
2023-10-30 14:35:54 -0400

    comments out validation routes

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,10 @@
 class Product < ApplicationRecord
-  validates :name, :price, presence: true
-  validates :name, uniqueness: true
-  validates :name, length: { in: 1..30 }
-  validates :price, numericality: { greater_than: 0 }
-  validates :description, length: { maximum: 500 }
-  #  validates :image, allow_blank: true, format: { with: %r{.(gif|jpg|png)\Z}i, message: "must be a URL for GIF, JPG or PNG image." }
+  # validates :name, :price, presence: true
+  # validates :name, uniqueness: true
+  # validates :name, length: { in: 1..30 }
+  # validates :price, numericality: { greater_than: 0 }
+  # validates :description, length: { maximum: 500 }
+  # validates :image, allow_blank: true, format: { with: %r{.(gif|jpg|png)\Z}i, message: "must be a URL for GIF, JPG or PNG image." }
 
   def is_discounted?
     if price <= 10
