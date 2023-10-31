770a1e8f44467667392d78cbc8f3c379bc242225
Derek McGee <dmcgee2001@gmail.com>
2023-10-31 13:29:30 -0400

    fixes tests

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -14,7 +14,7 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     assert_response 200
 
     data = JSON.parse(response.body)
-    assert_equal ["id", "name", "price", "image_url", "description", "inventory", "created_at", "updated_at"], data.keys
+    assert_equal ["id", "name", "price", "image_url", "description", "inventory", "supplier", "created_at", "updated_at"], data.keys
   end
   test "create" do
     assert_difference "Product.count", 1 do
