b255e36e31cf0f9f5a10a15d3a96b5dcc4ee3f41
Derek McGee <dmcgee2001@gmail.com>
2023-10-31 15:12:54 -0400

    fixes tests to take in new associations

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -14,6 +14,7 @@ class ProductsController < ApplicationController
       name: params["name"],
       price: params["price"],
       description: params["description"],
+      supplier_id: params["supplier_id"],
     )
     @product.save
     if @product.valid?
@@ -29,6 +30,7 @@ class ProductsController < ApplicationController
       name: params["name"] || @product.name,
       price: params["price"] || @product.price,
       description: params["description"] || @product.description,
+      supplier_id: params["supplier_id"] || @product.supplier_id,
     )
     if @product.valid?
       render :show
