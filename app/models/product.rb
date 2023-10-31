9ca78b86a0192b31175b603eb174f719b8b02f20
Derek McGee <dmcgee2001@gmail.com>
2023-10-31 14:59:01 -0400

    adds shortcuts for association  methods

--- app/models/product.rb
+++ app/models/product.rb
@@ -24,11 +24,12 @@ class Product < ApplicationRecord
     return total
   end
 
-  def supplier
-    Supplier.find_by(id: supplier_id)
-  end
-
-  def images
-    Image.where(product_id: id)
-  end
+  belongs_to :supplier
+  # def supplier
+  #   Supplier.find_by(id: supplier_id)
+  # end
+  has_many :images
+  # def images
+  #   Image.where(product_id: id)
+  # end
 end
