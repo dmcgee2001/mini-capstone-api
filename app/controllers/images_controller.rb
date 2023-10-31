5f4cc257af17d4a960e42c0baf03b364ce3b7a6c
Derek McGee <dmcgee2001@gmail.com>
2023-10-31 16:44:47 -0400

    adds image controller methods

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,2 +1,34 @@
-class ImagesController < ApplicationController
+class ImagesController < ApplicationControllerclass
+  def show
+    @image = Image.find_by(id: params["id"])
+    render :show
+  end
+
+  def index
+    @images = image.all
+    render :index
+  end
+
+  def create
+    @image = Image.create(
+      :url => params["url"],
+      :product_id => params["product_id"],
+    )
+    render :show
+  end
+
+  def update
+    @image = Image.find_by(id: params["id"])
+    @image.update(
+      :url => params["url"] || @image.url,
+      :product_id => params["product_id"] || @image.product_id,
+    )
+    render :show
+  end
+
+  def destroy
+    image = Image.find_by(id: params["id"])
+    image.delete
+    render json: { message: "successfully deleted" }
+  end
 end
