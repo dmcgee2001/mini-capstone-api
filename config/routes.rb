99fc5a5be09b36f7aadb5ec6f55c311ad08518f6
Derek McGee <dmcgee2001@gmail.com>
2023-10-31 12:41:56 -0400

    adds routes and controller actions for suppliers

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
+  # For Products
   get "/products/:id" => "products#show"
   get "/products" => "products#index"
   post "/products" => "products#create"
   patch "/products/:id" => "products#update"
   delete "/products/:id" => "products#destroy"
+
+  # For Suppliers
+  get "/suppliers/:id" => "suppliers#show"
+  get "/suppliers" => "suppliers#index"
+  post "/suppliers" => "suppliers#create"
+  patch "/suppliers/:id" => "suppliers#update"
+  delete "/suppliers/:id" => "suppliers#destroy"
 end
