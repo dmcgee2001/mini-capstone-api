22b14657b912b4d34ae29bc182e8bf0be71f8290
Derek McGee <dmcgee2001@gmail.com>
2023-10-31 13:51:21 -0400

    adds images to image model and attaches url and product id

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,10 +4,18 @@
 #   {name: "Staples", email: "staples@wahwah.com", phone_number: "(614)-273-8901"}
 # ])
 # Product.create!([
-#   {name: "car", price: "29000.0", image_url: "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/eqe-sedan/all-vehicles/2023-EQE350-SEDAN-AVP-DR.png", description: "this product can get you from place to place", inventory: 1, supplier_id: 2},
-#   {name: "book", price: "35.0", image_url: "https://www.collinsdictionary.com/images/full/book_181404689_1000.jpg", description: "good for reading and whatnot", inventory: 10, supplier_id: 1},
-#   {name: "keyboard", price: "35.0", image_url: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MQ052?wid=4000&hei=1800&fmt=jpeg&qlt=95&.v=1495129815011", description: "good for typing and whatnot", inventory: 4, supplier_id: 1},
-#   {name: "pencil", price: "2.0", image_url: "https://musgravepencil.com/cdn/shop/products/Havest_CedarPencil_1024x1024@2x.jpg?v=1576593212", description: "this product can write and erase", inventory: 5, supplier_id: 3},
-#   {name: "whiteboard", price: "20.0", image_url: "https://media.officedepot.com/images/f_auto,q_auto,e_sharpen,h_450/products/698542/698542_o01_110420/698542", description: "this product can be drawn on with eraseable marker", inventory: 3, supplier_id: 3},
-#   {name: "desk", price: "55.0", image_url: "https://cdn-images.article.com/products/SKU19375/2890x1500/image84734.jpg", description: "good for putting stuff on", inventory: 8, supplier_id: 3}
+#   {name: "car", price: "29000.0", description: "this product can get you from place to place", inventory: 1, supplier_id: 2},
+#   {name: "book", price: "35.0", description: "good for reading and whatnot", inventory: 10, supplier_id: 1},
+#   {name: "keyboard", price: "35.0", description: "good for typing and whatnot", inventory: 4, supplier_id: 1},
+#   {name: "pencil", price: "2.0", description: "this product can write and erase", inventory: 5, supplier_id: 3},
+#   {name: "whiteboard", price: "20.0", description: "this product can be drawn on with eraseable marker", inventory: 3, supplier_id: 3},
+#   {name: "desk", price: "55.0", description: "good for putting stuff on", inventory: 8, supplier_id: 3}
+# ])
+# Image.create!([
+#   {url: "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/eqe-sedan/all-vehicles/2023-EQE350-SEDAN-AVP-DR.png", product_id: 3},
+#   {url: "https://www.collinsdictionary.com/images/full/book_181404689_1000.jpg", product_id: 4},
+#   {url: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MQ052?wid=4000&hei=1800&fmt=jpeg&qlt=95&.v=1495129815011", product_id: 10},
+#   {url: "https://musgravepencil.com/cdn/shop/products/Havest_CedarPencil_1024x1024@2x.jpg?v=1576593212", product_id: 1},
+#   {url: "https://media.officedepot.com/images/f_auto,q_auto,e_sharpen,h_450/products/698542/698542_o01_110420/698542", product_id: 2},
+#   {url: "https://cdn-images.article.com/products/SKU19375/2890x1500/image84734.jpg", product_id: 9}
 # ])
