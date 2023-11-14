1f260a2e402fa9a623b5a6fa16ef5582899f0597
Derek McGee <dmcgee2001@gmail.com>
2023-11-14 13:54:13 -0500

    comments out seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,27 +1,27 @@
-Supplier.create!([
-  { name: "Amazon", email: "amazon@wahwah.com", phone_number: "1(800)-555-5555" },
-  { name: "Ford", email: "ford@wahwah.com", phone_number: "(613)-353-2917" },
-  { name: "Staples", email: "staples@wahwah.com", phone_number: "(614)-273-8901" },
-])
-Product.create!([
-  { name: "car", price: "29000.0", description: "this product can get you from place to place", inventory: 1, supplier_id: 2 },
-  { name: "book", price: "35.0", description: "good for reading and whatnot", inventory: 10, supplier_id: 1 },
-  { name: "keyboard", price: "35.0", description: "good for typing and whatnot", inventory: 4, supplier_id: 1 },
-  { name: "pencil", price: "2.0", description: "this product can write and erase", inventory: 5, supplier_id: 3 },
-  { name: "whiteboard", price: "20.0", description: "this product can be drawn on with eraseable marker", inventory: 3, supplier_id: 3 },
-  { name: "desk", price: "55.0", description: "good for putting stuff on", inventory: 8, supplier_id: 3 },
-])
-Image.create!([
-  { url: "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/eqe-sedan/all-vehicles/2023-EQE350-SEDAN-AVP-DR.png", product_id: 3 },
-  { url: "https://www.collinsdictionary.com/images/full/book_181404689_1000.jpg", product_id: 4 },
-  { url: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MQ052?wid=4000&hei=1800&fmt=jpeg&qlt=95&.v=1495129815011", product_id: 10 },
-  { url: "https://musgravepencil.com/cdn/shop/products/Havest_CedarPencil_1024x1024@2x.jpg?v=1576593212", product_id: 1 },
-  { url: "https://media.officedepot.com/images/f_auto,q_auto,e_sharpen,h_450/products/698542/698542_o01_110420/698542", product_id: 2 },
-  { url: "https://cdn-images.article.com/products/SKU19375/2890x1500/image84734.jpg", product_id: 9 },
-  { url: "https://cdn.vectorstock.com/i/preview-1x/30/25/pencil-vector-39133025.jpg", product_id: 1 },
-])
+# Supplier.create!([
+#   { name: "Amazon", email: "amazon@wahwah.com", phone_number: "1(800)-555-5555" },
+#   { name: "Ford", email: "ford@wahwah.com", phone_number: "(613)-353-2917" },
+#   { name: "Staples", email: "staples@wahwah.com", phone_number: "(614)-273-8901" },
+# ])
+# Product.create!([
+#   { name: "car", price: "29000.0", description: "this product can get you from place to place", inventory: 1, supplier_id: 2 },
+#   { name: "book", price: "35.0", description: "good for reading and whatnot", inventory: 10, supplier_id: 1 },
+#   { name: "keyboard", price: "35.0", description: "good for typing and whatnot", inventory: 4, supplier_id: 1 },
+#   { name: "pencil", price: "2.0", description: "this product can write and erase", inventory: 5, supplier_id: 3 },
+#   { name: "whiteboard", price: "20.0", description: "this product can be drawn on with eraseable marker", inventory: 3, supplier_id: 3 },
+#   { name: "desk", price: "55.0", description: "good for putting stuff on", inventory: 8, supplier_id: 3 },
+# ])
+# Image.create!([
+#   { url: "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/eqe-sedan/all-vehicles/2023-EQE350-SEDAN-AVP-DR.png", product_id: 3 },
+#   { url: "https://www.collinsdictionary.com/images/full/book_181404689_1000.jpg", product_id: 4 },
+#   { url: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MQ052?wid=4000&hei=1800&fmt=jpeg&qlt=95&.v=1495129815011", product_id: 10 },
+#   { url: "https://musgravepencil.com/cdn/shop/products/Havest_CedarPencil_1024x1024@2x.jpg?v=1576593212", product_id: 1 },
+#   { url: "https://media.officedepot.com/images/f_auto,q_auto,e_sharpen,h_450/products/698542/698542_o01_110420/698542", product_id: 2 },
+#   { url: "https://cdn-images.article.com/products/SKU19375/2890x1500/image84734.jpg", product_id: 9 },
+#   { url: "https://cdn.vectorstock.com/i/preview-1x/30/25/pencil-vector-39133025.jpg", product_id: 1 },
+# ])
 
-User.create!([
-  { name: "Derek McGee", email: "derek@example.com", password: "blahblah", password_confirmation: "blahblah" },
-  { name: "Kalista Pineda", email: "kalista@example.com", password: "yoyoyo", password_confirmation: "yoyoyo" },
-])
+# User.create!([
+#   { name: "Derek McGee", email: "derek@example.com", password: "blahblah", password_confirmation: "blahblah" },
+#   { name: "Kalista Pineda", email: "kalista@example.com", password: "yoyoyo", password_confirmation: "yoyoyo" },
+# ])
