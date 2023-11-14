3688e473335d94ca3c79f7b0b048c134399d74c2
Derek McGee <dmcgee2001@gmail.com>
2023-11-14 14:42:31 -0500

    adds inventory to controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,6 +7,7 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
     render :index
+    pp current_user
   end
 
   def create
@@ -14,6 +15,7 @@ class ProductsController < ApplicationController
       name: params["name"],
       price: params["price"],
       description: params["description"],
+      description: params["inventory"],
       supplier_id: params["supplier_id"],
     )
     @product.save
@@ -30,6 +32,7 @@ class ProductsController < ApplicationController
       name: params["name"] || @product.name,
       price: params["price"] || @product.price,
       description: params["description"] || @product.description,
+      description: params["inventory"] || @product.inventory,
       supplier_id: params["supplier_id"] || @product.supplier_id,
     )
     if @product.valid?
