6730b3b178de79882878d8c3b356adce4e013518
Derek McGee <dmcgee2001@gmail.com>
2023-11-15 13:18:57 -0500

    adds jbuilder and create function for order controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,13 @@
 class OrdersController < ApplicationController
+  def create
+    @user = User.create!(
+      user_id: params["user_id"],
+      product_id: params["product_id"],
+      quantity: params["quantity"],
+      subtotal: params["subtotal"],
+      tax: params["tax"],
+      total: params["total"],
+
+    )
+  end
 end
