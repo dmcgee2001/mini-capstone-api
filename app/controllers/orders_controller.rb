7497f55ec6b257a804002e72899b24c2bb71523b
Derek McGee <dmcgee2001@gmail.com>
2023-11-15 15:22:04 -0500

    adjusts controller to be cleaner for subtotal, tax, and total values

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,14 +1,16 @@
 class OrdersController < ApplicationController
   def create
     product = Product.find(params["product_id"])
-    price = product.price
+    calculated_subtotal = product.price * params[:quantity].to_i
+    calculated_tax = (product.price * 0.09)
+    calculated_total = (calculated_subtotal * 1.09)
     @order = Order.create!(
       user_id: current_user.id,
       product_id: params["product_id"],
       quantity: params["quantity"],
-      subtotal: price * params["quantity"].to_i,
-      tax: (product.tax * params["quantity"].to_i),
-      total: (product.total * params["quantity"].to_i),
+      subtotal: calculated_subtotal,
+      tax: calculated_tax,
+      total: calculated_total,
 
     )
     render :show
