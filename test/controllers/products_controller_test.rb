ee231b48af3ebe57bca40c0f379c49ba613375f9
Derek McGee <dmcgee2001@gmail.com>
2023-11-16 14:42:18 -0500

    adds admin to products controller test to update create and destroy

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,6 +1,13 @@
 require "test_helper"
 
 class ProductsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @user = User.create(name: "Test", email: "test@test.com", password: "password", admin: true)
+    post "/sessions.json", params: { email: "test@test.com", password: "password" }
+    data = JSON.parse(response.body)
+    @jwt = data["jwt"]
+  end
+
   test "index" do
     get "/products.json"
     assert_response 200
@@ -18,12 +25,12 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   end
   test "create" do
     assert_difference "Product.count", 1 do
-      post "/products.json", params: { name: "test product", price: 1, description: "test description", supplier_id: Supplier.first.id }
+      post "/products.json", params: { name: "test product", price: 1, description: "test description", supplier_id: Supplier.first.id }, headers: { "Authorization" => "Bearer #{@jwt}" }
     end
   end
   test "update" do
     product = Product.first
-    patch "/products/#{product.id}.json", params: { name: "Updated name" }
+    patch "/products/#{product.id}.json", params: { name: "Updated name" }, headers: { "Authorization" => "Bearer #{@jwt}" }
     assert_response 200
 
     data = JSON.parse(response.body)
@@ -31,7 +38,7 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   end
   test "destroy" do
     assert_difference "Product.count", -1 do
-      delete "/products/#{Product.first.id}.json"
+      delete "/products/#{Product.first.id}.json", headers: { "Authorization" => "Bearer #{@jwt}" }
       assert_response 200
     end
   end
