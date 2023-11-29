76cdc8ca274edae386a32a36c8e289bcb9987c4e
Derek McGee <dmcgee2001@gmail.com>
2023-11-29 13:45:00 -0500

    adds view template for carted products

--- app/controllers/carted_products_controller.rb
+++ app/controllers/carted_products_controller.rb
@@ -1,32 +1,30 @@
 class CartedProductsController < ApplicationController
-  class ProductsController < ApplicationController
-    before_action :authenticate_user
+  before_action :authenticate_user
 
-    def index
-      @carted_products = CartedProduct.all
-      render :index
-    end
+  def index
+    @carted_products = current_user.carted_products
+    render :index
+  end
 
-    def create
-      @carted_product = CartedProduct.new(
-        user_id: params["user_id"],
-        product_id: params["product_id"],
-        quantity: params["quantity"],
-        status: params["stauts"],
-        order_id: params["order_id"],
-      )
-      @carted_product.save
-      if @carted_product.valid?
-        render :show
-      else
-        render json: { errors: @carted_product.errors.full_messages }, status: :unprocessable_entity
-      end
+  def create
+    @carted_product = CartedProduct.new(
+      user_id: current_user.id,
+      product_id: params["product_id"],
+      quantity: params["quantity"],
+      status: "carted",
+      order_id: nil,
+    )
+    @carted_product.save
+    if @carted_product.valid?
+      render :show
+    else
+      render json: { errors: @carted_product.errors.full_messages }, status: :unprocessable_entity
     end
+  end
 
-    def destroy
-      carted_product = CartedProduct.find_by(id: params["id"])
-      carted_product.delete
-      render json: { message: "successfully deleted" }
-    end
+  def destroy
+    carted_product = CartedProduct.find_by(id: params["id"])
+    carted_product.delete
+    render json: { message: "successfully deleted" }
   end
 end
