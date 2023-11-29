4a64ec29d3a602a604d6a501d0de3dc6d55f4702
Derek McGee <dmcgee2001@gmail.com>
2023-11-29 13:34:14 -0500

    adds index, create, and destroy methods and routes for carted products

--- app/controllers/carted_products_controller.rb
+++ app/controllers/carted_products_controller.rb
@@ -1,2 +1,32 @@
 class CartedProductsController < ApplicationController
+  class ProductsController < ApplicationController
+    before_action :authenticate_user
+
+    def index
+      @carted_products = CartedProduct.all
+      render :index
+    end
+
+    def create
+      @carted_product = CartedProduct.new(
+        user_id: params["user_id"],
+        product_id: params["product_id"],
+        quantity: params["quantity"],
+        status: params["stauts"],
+        order_id: params["order_id"],
+      )
+      @carted_product.save
+      if @carted_product.valid?
+        render :show
+      else
+        render json: { errors: @carted_product.errors.full_messages }, status: :unprocessable_entity
+      end
+    end
+
+    def destroy
+      carted_product = CartedProduct.find_by(id: params["id"])
+      carted_product.delete
+      render json: { message: "successfully deleted" }
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -34,4 +34,9 @@ Rails.application.routes.draw do
   post "/orders" => "orders#create"
   get "/orders/:id" => "orders#show"
   get "/orders/" => "orders#index"
+
+  # For Carted Products
+  get "/carted_products" => "carted_products#index"
+  post "/carted_products" => "carted_products#create"
+  delete "/carted_products/:id" => "carted_products#destroy"
 end
