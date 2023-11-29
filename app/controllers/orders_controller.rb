5789d505980edfd15478c364dc6b5b8c83573b3c
Derek McGee <dmcgee2001@gmail.com>
2023-11-29 14:40:34 -0500

    adds loop to orders create action to find totals prices of all carted items

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,17 +2,26 @@ class OrdersController < ApplicationController
   before_action :authenticate_user
 
   def create
-    product = Product.find(params["product_id"])
-    calculated_subtotal = product.price * params[:quantity].to_i
-    calculated_tax = (product.price * 0.09)
-    calculated_total = (calculated_subtotal * 1.09)
+    products = current_user.carted_products.where(status: "carted")
+    index = 0
+    cart_subtotal = 0
+    cart_tax = 0
+    while index < products.length
+      v_id = products[index][:product_id]
+      product = Product.find_by(id: v_id)
+      calculated_subtotal = product.price
+      calculated_tax = (product.price * 0.09)
+      calculated_total = (calculated_subtotal + calculated_tax)
+      cart_subtotal += calculated_subtotal
+      cart_tax += calculated_tax
+      index += 1
+    end
+    cart_total = cart_subtotal + cart_tax
     @order = Order.create!(
       user_id: current_user.id,
-      product_id: params["product_id"],
-      quantity: params["quantity"],
-      subtotal: calculated_subtotal,
-      tax: calculated_tax,
-      total: calculated_total,
+      subtotal: cart_subtotal,
+      tax: cart_tax,
+      total: cart_total,
     )
     render :show
   end
