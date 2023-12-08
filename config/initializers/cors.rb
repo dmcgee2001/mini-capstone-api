407112a5bfe2db1c5bbd29bd89d474b635ac67a8
Derek McGee <dmcgee2001@gmail.com>
2023-12-08 14:10:41 -0500

    allows connection from render site

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,7 +2,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "mini-capstone.peterxjang.com", "localhost:5173"
+    origins "mini-capstone.peterxjang.com", "localhost:5173", "https://mini-capstone-frontend-li7u.onrender.com"
     resource "*", headers: :any, methods: [:get, :post, :patch, :put, :delete]
   end
 end
